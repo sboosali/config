015b148a0cd952e81c0297fcf4f060e8c9410690
Spiros Boosalis <samboosalis@gmail.com>
2016-06-01 00:31:57 +0300

    .

---
+++ pull-repos.sh
@@ -0,0 +1,24 @@
+git clone https://github.com/sboosali/commands
+git clone https://github.com/sboosali/commands-core
+git clone https://github.com/sboosali/commands-frontends-dgn13
+# git clone https://github.com/sboosali/commands-frontends-wsr
+git clone https://github.com/sboosali/commands-backends-osx9
+# git clone https://github.com/sboosali/commands-backends-win10
+git clone https://github.com/sboosali/commands-spiros
+
+git clone https://github.com/sboosali/workflow
+git clone https://github.com/sboosali/workflow-types
+# git clone https://github.com/sboosali/workflow-linux
+git clone https://github.com/sboosali/workflow-osx
+git clone https://github.com/sboosali/workflow-windows
+git clone https://github.com/sboosali/workflow-extra
+
+git clone https://github.com/sboosali/enumerate
+git clone https://github.com/sboosali/enumerate-functions
+
+git clone https://github.com/sboosali/export
+
+git clone https://github.com/sboosali/vinyl
+git clone https://github.com/sboosali/Earley
+
+# git clone https://github.com/sboosali/
