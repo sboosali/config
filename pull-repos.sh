7e133d5c80eac7eeaabfc6da661f22d1b8dc3f54
Spiros Boosalis <samboosalis@gmail.com>
2016-09-06 21:25:14 +0300

    Add dependencies for commands

--- pull-repos.sh
+++ pull-repos.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 
 ## clone
+# git clone https://github.com/sboosali/
+
+# Commands dependences
 
 git clone https://github.com/sboosali/commands
 git clone https://github.com/sboosali/commands-core
 git clone https://github.com/sboosali/commands-frontends-dgn13
-# git clone https://github.com/sboosali/commands-frontends-wsr
 git clone https://github.com/sboosali/commands-backends-osx9
-# git clone https://github.com/sboosali/commands-backends-win10
 git clone https://github.com/sboosali/commands-spiros
 
 git clone https://github.com/sboosali/workflow
 git clone https://github.com/sboosali/workflow-types
-# git clone https://github.com/sboosali/workflow-linux
 git clone https://github.com/sboosali/workflow-osx
 git clone https://github.com/sboosali/workflow-windows
 git clone https://github.com/sboosali/workflow-extra
@@ -20,12 +20,20 @@ git clone https://github.com/sboosali/workflow-extra
 git clone https://github.com/sboosali/enumerate
 git clone https://github.com/sboosali/enumerate-functions
 
-git clone https://github.com/sboosali/export
+git clone https://github.com/sboosali/derive-monoid
+git clone https://github.com/sboosali/s-expression
+git clone https://github.com/sboosali/reified-bindings
 
+# Other projects
+
+git clone https://github.com/sboosali/export
 git clone https://github.com/sboosali/vinyl
 git clone https://github.com/sboosali/Earley
 
-# git clone https://github.com/sboosali/
+# `#` isn't comment in batch 
+# git clone https://github.com/sboosali/commands-frontends-wsr
+# git clone https://github.com/sboosali/commands-backends-win10
+# git clone https://github.com/sboosali/workflow-linux
 
 ## https://help.github.com/articles/remove-sensitive-data/
 # [dangerous] git filter-branch --force --index-filter 'git rm -r --cached --ignore-unmatch PATH-TO-YOUR-FILE-WITH-SENSITIVE-DATA' --prune-empty --tag-name-filter cat -- --all
